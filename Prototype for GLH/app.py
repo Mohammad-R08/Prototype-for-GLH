06e60e9530429f4735a49fab801a38bad3697b19
Mohammad-R08 <Mohammadrashid07052008@gmail.com>
2026-05-05 13:39:20 +0100

    Update app.py

--- Prototype for GLH/app.py
+++ Prototype for GLH/app.py
@@ -86,7 +86,7 @@ def register():
         cur.close()
         conn.close()
 
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
 
     return render_template('register.html')
 
